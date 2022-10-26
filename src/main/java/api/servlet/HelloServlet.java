92de7903e8d3d3ef4b9e59dee74742c52d4cbebb
119717661 <119717661@umail.ucc.ie>
2022-10-26 18:15:05 +0100

    NewUpdates

--- src/main/java/api/servlet/HelloServlet.java
+++ src/main/java/api/servlet/HelloServlet.java
@@ -17,65 +17,15 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Business Logic
-        UserModel model = new UserModel();
-        User user = model.getUser();
-
-//        user.setId("1111");
-//        user.setName("Up1");
-
-        // Create response message
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getOutputStream(), user);
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse response)
             throws ServletException, IOException {
         // Convert request to Java object
-        ObjectMapper mapper = new ObjectMapper();
-        User sendUser = mapper.readValue(req.getInputStream(), User.class);
 
-        // Business Logic
-        User user = new User("");
-        user.setId("0000");
-        user.setName("Response from POST form " + sendUser.getName());
-
-        // Create response message
-        mapper.writeValue(response.getOutputStream(), user);
 
     }
 
 }
 
-class User {
-    private String id;
-    private String name;
-
-    public User(String id) {
-        this.id = id;
-    }
-
-    public User(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-}
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -8,7 +8,7 @@ public class HelloTest {
     @Test
     public void hello_first_test_case() {
         assertTrue(true);
-//        fail("เขียนชุดการทดสอบสิ");
+//        fail("lol");
     }
 
 }
